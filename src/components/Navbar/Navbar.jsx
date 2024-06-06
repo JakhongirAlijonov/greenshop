1655df32b8078caa04f3313b68cece6409ebe2bc
JakhongirAlijonov <jakhongirweb@gmail.com>
2024-06-06 22:18:33 +0500

    navbar half ready

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,51 @@
-
+import { Link } from "react-router-dom"
+import './Navbar.css'
+import { useState } from "react"
 function Navbar() {
+  const [ isOpen , setIsOpen ] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
   return (
-    <div >
-      
+    <div className="site-header container" >
+    <Link to={'/'}>
+      <img src="./public/logo.svg" alt="site logo" />
+    </Link>
+    <ul className="header-list">
+      <li className="header-item">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="header-item">
+        <Link to="about">About</Link>
+      </li>
+      <li className="header-item page-show">
+        <span  onClick={ ()=> handleClick() } >Pages 🔽</span>
+        {
+          isOpen &&
+           <div className="hidden-links"> 
+           <Link to={'/service'} onClick={handleLinkClick}>Service</Link>
+              <Link to={'/portfolio'} onClick={handleLinkClick}>Portfolio</Link>
+              <Link to={'/team'} onClick={handleLinkClick}>Team</Link>
+           </div>
+        }
+      </li>
+      <li className="header-item">
+        <Link to="shop">Shop</Link>
+      </li>
+      <li className="header-item">
+        <Link to="news">News</Link>
+      </li>
+    </ul>
+
+
+    <div className="user">
+      <button>User</button>
+    </div>
     </div>
   )
 }
---
+++ src/firebase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcyB92_HUa6qRFbbPNpjd5zA4LS1euYCs",
+  authDomain: "organic-48388.firebaseapp.com",
+  projectId: "organic-48388",
+  storageBucket: "organic-48388.appspot.com",
+  messagingSenderId: "402604816966",
+  appId: "1:402604816966:web:71591c2455505802386847"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
