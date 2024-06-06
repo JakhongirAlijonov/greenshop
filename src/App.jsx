933c9a682fe9f3b418f042000e24576cdeef4db0
JakhongirAlijonov <jakhongirweb@gmail.com>
2024-06-06 19:56:26 +0500

    new files and folder sturctuer has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css'
-
-
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Shop from './pages/Shop/Shop'
+import News from './pages/News/News'
+import Contact from './pages/Contact/Contact'
+import Team from './pages/Team/Team'
+import Portfolio from './pages/Portfolio/Portfolio'
+import Service from './pages/Service/Service'
 function App() {
   return (
-    <div>App</div>
+   <Router>
+   <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='shop' element={<Shop/>}/>
+      <Route path='news' element={<News/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/service' element={<Service/>}/>
+      <Route path='/portfolio' element={<Portfolio/>}/>
+      <Route path='/team' element={<Team/>}/>
+
+
+    </Routes>
+    <Footer/>
+   </Router>
   )
 }
 
